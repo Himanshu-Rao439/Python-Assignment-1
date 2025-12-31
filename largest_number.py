3ec9181de08cdbc98853dd1f901024d5329f884a
Himanshu Rao <himanshurao061@gmail.com>
2025-12-31 18:58:54 +0530

    Update largest_number.py

--- largest_number.py
+++ largest_number.py
@@ -1,11 +1,15 @@
-
 def largest_number(lst):
-    largest = lst[0]
-    for num in lst:
-        if num > largest:
-            largest = num
+    i = 0                 # index start
+    largest = lst[0]      # assume first number is largest
+
+    while i < len(lst):   # jab tak list khatam na ho
+        if lst[i] > largest:
+            largest = lst[i]
+        i += 1            # next index
+
     return largest
 
-numbers = [10, 45, 3, 99, 23]
 
+numbers = [10, 45, 3, 99, 23]
 print(largest_number(numbers))
+
