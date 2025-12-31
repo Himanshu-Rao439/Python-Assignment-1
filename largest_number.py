9d1a8fe2e8c7e745ce6639241307206b7992a2bf
Himanshu Rao <himanshurao061@gmail.com>
2025-12-31 17:02:49 +0530

    Update largest_number.py

--- largest_number.py
+++ largest_number.py
@@ -1,4 +1,4 @@
-largest_number.py
+
 def largest_number(lst):
     largest = lst[0]
     for num in lst:
@@ -7,4 +7,5 @@ def largest_number(lst):
     return largest
 
 numbers = [10, 45, 3, 99, 23]
+
 print(largest_number(numbers))
